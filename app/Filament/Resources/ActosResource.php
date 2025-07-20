914c3f9246b01e09c22679e3346b8cce0d34c366
erickvnrm <erickvnrm@gmail.com>
2025-07-20 01:36:53 -0500

    test

--- app/Filament/Resources/ActosResource.php
+++ app/Filament/Resources/ActosResource.php
@@ -221,13 +221,6 @@ class ActosResource extends Resource
             ]);
     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-
     public static function getPages(): array
     {
         return [
